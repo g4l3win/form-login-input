e8b88ab3dd415d4e8184a888214094da68f5c163
Acer <Acer@LAPTOP-FTNPN94M>
2023-09-09 15:50:57 +0700

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "admin" And TextBox2.Text = "admin" Then
+            MessageBox.Show("Login Berhasil Selamat datang Admin")
+        Else MessageBox.Show("Password atau Username salah. Coba Lagi")
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.ResetText()
+        TextBox2.ResetText()
+
+    End Sub
+End Class
